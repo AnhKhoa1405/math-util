73e7f2670e78d169242a0b1ced5a58647c510dfc
AnhKhoa1405 <truonganhkhoa1405@gmail.com>
2020-11-19 08:34:06 +0700

    third time upload

--- src/khoata/util/MathUtility.java
+++ src/khoata/util/MathUtility.java
@@ -17,6 +17,21 @@ public class MathUtility {
     //hàm tính n! n>= 0 && n <= 20
     // nếu quá 20 thì tràn kiểu long
     //0! = 1; 1! = 1;
+//    public static long getFactorial(int n){
+//        //object là những gì cụ thể và class là nhóm mà object đó thuộc về
+//        if(n < 0 || n > 20){
+//           throw new IllegalArgumentException("n must be >= 0 & <= 20");
+//           //đưa đầu vào cà chớn, mình hok thèm trả về 1 giá ám chỉ sai rồi
+//        }
+//        if( n == 0 || n == 1){
+//            return 1;
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
     public static long getFactorial(int n){
         //object là những gì cụ thể và class là nhóm mà object đó thuộc về
         if(n < 0 || n > 20){
@@ -26,10 +41,8 @@ public class MathUtility {
         if( n == 0 || n == 1){
             return 1;
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        return n * getFactorial(n-1); //đệ quy đó em
+        // gọi lại chính mình với quy mô nhỏ hơn
+        // n! = n * (n - 1)!
     }
 }
